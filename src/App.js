9eeeceb06ed5c8efa37989d00a0c78d4a137de1f
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-09-01 18:35:58 +0900

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Top from "./screens/Top";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Top />
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div className="nav-wrapper green">
+        <a href="/" className="brand-logo center">
+          Tech Quiz
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
